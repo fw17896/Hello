bbbfc8d885204b7c2421e909f0c29aa55f088a94
fw17896 <fw7896@gmail.com>
2026-09-24 19:08:22 +0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins!");
         System.out.println("Java build and test successful.");
+        
     }
 }
