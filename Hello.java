ed2cd9e8c07ca49ee66f3caf8663b240007c2c0d
fw17896 <fw7896@gmail.com>
2026-09-24 20:15:37 +0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins!");
-        System.out.println("Hook.");        
+        System.out.println("Hook.");   
+        
     }
 }
